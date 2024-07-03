84b1001b67c6a582a70c4ff5d920ee104dc037b8
zihadx <blankzihad@gmail.com>
2024-07-03 10:51:46 +0600

    first commit and set up project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+        <h1 className='text-5xl font-bold text-center w-full mx-auto text-blue-700'>Hi </h1>
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
